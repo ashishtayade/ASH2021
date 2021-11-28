846727f3430126cf5e53aaab598e4068841c432f
Ashish Tayade <ashish.tayade4@gmail.com>
2021-11-28 22:18:15 +0530

    Add files via upload

---
+++ js/screensaver.js
@@ -0,0 +1,35 @@
+        let timer, currSeconds = 0;
+  
+        function resetTimer() {
+            /* Hide the timer text */
+            //document.querySelector(".timertext").style.display = 'none';
+            /* Clear the previous interval */
+            clearInterval(timer);
+            /* Reset the seconds of the timer */
+            currSeconds = 0;
+            /* Set a new interval */
+            timer = setInterval(startIdleTimer, 1000);
+            $('.screensaverDiv').addClass('d-none');
+        }
+  
+        // Define the events that
+        // would reset the timer
+        window.onload = resetTimer;
+        window.onmousemove = resetTimer;
+        window.onmousedown = resetTimer;
+        window.ontouchstart = resetTimer;
+        window.onclick = resetTimer;
+        window.onkeypress = resetTimer;
+  
+        function startIdleTimer() {
+            /* Increment the timer seconds */
+            currSeconds++;
+            /* Set the timer text
+                to the new value */
+            //document.querySelector(".secs").textContent = currSeconds;
+            /* Display the timer text */
+            //document.querySelector(".timertext").style.display = 'block';
+            if(currSeconds == 120) {
+              $('.screensaverDiv').removeClass('d-none');
+            }
+        }
