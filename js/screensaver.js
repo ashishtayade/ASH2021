7c2e8de4b28404d45ac180c8f941e9539a0d233a
Ashish Tayade <ashish.tayade4@gmail.com>
2021-12-01 18:55:32 +0530

    Add files via upload

--- js/screensaver.js
+++ js/screensaver.js
@@ -33,3 +33,7 @@
               $('.screensaverDiv').removeClass('d-none');
             }
         }
+
+        $( "body" ).dblclick(function() {
+             document.fullScreenElement && null !== document.fullScreenElement || !document.mozFullScreen && !document.webkitIsFullScreen ? document.documentElement.requestFullScreen ? document.documentElement.requestFullScreen() : document.documentElement.mozRequestFullScreen ? document.documentElement.mozRequestFullScreen() : document.documentElement.webkitRequestFullScreen && document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT) : document.cancelFullScreen ? document.cancelFullScreen() : document.mozCancelFullScreen ? document.mozCancelFullScreen() : document.webkitCancelFullScreen && document.webkitCancelFullScreen()
+        });
